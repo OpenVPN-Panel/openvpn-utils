c2b00fb105cec37e721c743f950ce8152b5452e8
Grigoriy <61056244+domster704@users.noreply.github.com>
2025-07-07 18:29:32 +0300

    Update delClient.sh

--- delClient.sh
+++ delClient.sh
@@ -1,32 +1,45 @@
 #!/bin/bash
 
 clientName=${1}
-adminDir=/home/admin
-caDir="$adminDir/easy-rsa/"
+adminDir=/home/admin/openvpn
+caDir="$adminDir/easy-rsa"
+openvpnServerDir="/etc/openvpn/server"
 
-# Search for files matching the client name
-matches=$(sudo find ~ -name "${clientName}*")
+if [ -z "$clientName" ]; then
+  echo "Error: Please specify the client name."
+  exit 1
+fi
+
+matches=$(sudo find "$adminDir" -name "${clientName}*")
 
 if [[ -n "$matches" ]]; then
     echo "The following files were found:"
     echo "$matches"
     echo
 
-    read -p "Are you sure you want to delete these files? [y/N]: " confirm
+    read -p "Are you sure you want to delete these files and revoke the certificate? [y/N]: " confirm
 
     if [[ "${confirm,,}" == "y" ]]; then
 
-        cd $caDir || exit 1
-        sudo bash ./easyrsa revoke $clientName
-        sudo bash ./easyrsa gen-crl
+        cd "$caDir" || { echo "Failed to change directory to $caDir"; exit 1; }
 
-        sudo cp $adminDir/easy-rsa/pki/crl.pem /etc/openvpn/server/
+        echo "Revoking client certificate..."
+        sudo ./easyrsa revoke "$clientName"
 
-        echo "Deleting..."
-        sudo find ~ -name "${clientName}*" -exec rm -f {} \;
-        echo "Files deleted successfully."
+        echo "Generating Certificate Revocation List (CRL)..."
+        sudo ./easyrsa gen-crl
 
+        sudo cp "$caDir/pki/crl.pem" "$openvpnServerDir/"
+        sudo chmod 644 "$openvpnServerDir/crl.pem"
+
+        echo "Deleting client-related files..."
+        sudo find "$adminDir" -name "${clientName}*" -exec rm -f {} \;
+
+        echo "Restarting OpenVPN server..."
         sudo systemctl restart openvpn-server@server.service
+
+        echo "Files deleted and certificate revoked successfully."
+
     else
         echo "Deletion cancelled."
     fi
